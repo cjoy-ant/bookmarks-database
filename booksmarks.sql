3843a64057935d23a25fa164feb34ea6547a0ba2
Christine Antonio <christinejm.antonio@gmail.com>
2021-03-17 14:54:50 -0700

    fix type - id 2

--- booksmarks.sql
+++ booksmarks.sql
@@ -16,4 +16,4 @@ insert into bookmarks (id, title, url, rating, description)
 values 
   (0, 'Google', 'http://www.google.com', 3, 'Internet-related services and products.'),
   (1, 'Thinkful', 'http://www.thinkful.com', 5, '1-on-1 learning to accelerate your way to a new high-growth tech career!'),
-  (3, 'Github', 'http://www.github.com', 4, `brings together the world's largest community of developers.`);
+  (2, 'Github', 'http://www.github.com', 4, `brings together the world's largest community of developers.`);
